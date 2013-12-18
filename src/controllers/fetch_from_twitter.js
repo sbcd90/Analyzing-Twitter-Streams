13f0cb42043efa1bd952794e84359e89af3726d3
I076326 <subhobrata.dey@sap.com>
2013-12-18 12:46:24 +0530

    small fixes for Twitter fetch

--- src/controllers/fetch_from_twitter.js
+++ src/controllers/fetch_from_twitter.js
@@ -9,23 +9,24 @@ exports.get = function(req,res){
     null,
     'HMAC-SHA1'
   );
+  var prev_tweet = '';
   setInterval(function(){
     oauth.get(
-      'https://api.twitter.com/1.1/search/tweets.json?q=%23mother&result_type=mixed&count=1',
+      'https://api.twitter.com/1.1/search/tweets.json?q=%23mother&result_type=recent&count=1',
       '846948925-wwZw1TFbAGmaGsADH9WipmMXwM31gEUaiMk7RUsW',
       'q3uUx8YefbyOzeMXmCa91OO4DomDaefUBDwUZnQ',
       function(e, data, res){
-	if(e) console.error(e);
-
-	var cleandata = JSON.parse(data);
-	var tweet_id = cleandata.statuses[0].id_str;
-	var text = cleandata.statuses[0].text;
-	var user_id = cleandata.statuses[0].user.id_str;
-	var geo_location = cleandata.statuses[0].geo;
-	var coordinates = cleandata.statuses[0].coordinates;
-	var place = cleandata.statuses[0].place;
-
-	require('../models/mongolab_connect').push_to_db(tweet_id,text,user_id,geo_location,coordinates,place);
+	     if(e) console.error(e);
+	     var cleandata = JSON.parse(data);
+	     var tweet_id = cleandata.statuses[0].id_str;
+	     var text = cleandata.statuses[0].text;
+	     var user_id = cleandata.statuses[0].user.id_str;
+	     var geo_location = cleandata.statuses[0].geo;
+	     var coordinates = cleandata.statuses[0].coordinates;
+	     var place = cleandata.statuses[0].place;
+       if(prev_tweet==tweet_id)
+	       require('../models/mongolab_connect').push_to_db(tweet_id,text,user_id,geo_location,coordinates,place);
+       prev_tweet = tweet_id;
       });
   },180000);
 }
